d7a245b7aabe8b35960e7ae77f3d25baa6ed8120
A. Sebastian Dietzel <albsen@gmail.com>
2026-06-14 00:16:47 +0400

    blacklisted modules with security issues and lsm

--- modules/x1e80100.nix
+++ modules/x1e80100.nix
@@ -22,6 +22,8 @@ let
     "splash"
     "console=tty0"
     "crashkernel=2G-4G:320M,4G-32G:512M,32G-64G:1024M,64G-128G:2048M,128G-:4096M"
+    "module_blacklist=algif_aead,esp4,esp6,rxrpc"
+    "lsm=landlock,yama,bpf"
   ];
 in
 {
