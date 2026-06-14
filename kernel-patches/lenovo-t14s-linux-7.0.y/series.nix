8dd0da603665952c383ae6040ffdbd1cdcf7ab96
A. Sebastian Dietzel <albsen@gmail.com>
2026-06-14 16:08:35 +0400

    added missing kernel patches

---
+++ kernel-patches/lenovo-t14s-linux-7.0.y/series.nix
@@ -0,0 +1,54 @@
+[
+  {
+    name = "jg-d0f38cd09a44-dp-audio-inactive";
+    patch = ./0002-drm-msm-dp-Do-not-fail-audio-prepare-when-DP-is-inac.patch;
+  }
+  {
+    name = "jg-8d1b68a92a50-sc8280xp-blackrock-usb-phy-supply";
+    patch = ./0005-arm64-dts-qcom-sc8280xp-blackrock-correct-usb-phy-po.patch;
+  }
+  {
+    name = "jg-ceab637ac377-x1-el2-pas";
+    patch = ./0008-arm64-dts-qcom-x1-Add-x1-el2-pas.dtso-for-PAS-manage.patch;
+  }
+  {
+    name = "jg-6fcd1cf574ba-lpass-wsa-pm-clock";
+    patch = ./0014-ASoC-codecs-lpass-wsa-macro-Switch-to-PM-clock-frame.patch;
+  }
+  {
+    name = "jg-c7db8e3f6c44-lpass-va-pm-clock";
+    patch = ./0015-ASoC-codecs-lpass-va-macro-Switch-to-PM-clock-framew.patch;
+  }
+  {
+    name = "jg-6ad5d0d09a8d-sc8280xp-dispcc-mdp";
+    patch = ./0016-clk-qcom-dispcc-sc8280xp-Don-t-park-mdp_clk_src-at-r.patch;
+  }
+  {
+    name = "jg-d62c180a4dab-surface-pro-12-panel";
+    patch = ./0017-drm-panel-edp-Add-surface-pro-12inch-panel-NE120DRM-.patch;
+  }
+  {
+    name = "jg-72b34bed8486-thinkbook-16-dp-dais";
+    patch = ./0019-arm64-dts-qcom-x1p42100-lenovo-thinkbook-16-add-disp.patch;
+  }
+  {
+    name = "jg-e84c55ce8316-thinkbook-16-gpi-dma";
+    patch = ./0020-arm64-dts-qcom-x1p42100-lenovo-thinkbook-16-enable-g.patch;
+  }
+  {
+    name = "jg-b54035cd56ee-panel-n140jce-el1";
+    patch = ./0021-drm-panel-edp-add-panel-CMN-0x1476-N140JCE-EL1.patch;
+  }
+  {
+    name = "jg-5874b0aff67b-pmic5-gen3-adc-thermal";
+    patch = ./0023-thermal-qcom-add-support-for-PMIC5-Gen3-ADC-thermal-.patch;
+  }
+  {
+    name = "jg-bd9bfcbca69c-pmic5-gen3-adc";
+    patch = ./0024-iio-adc-Add-support-for-QCOM-PMIC5-Gen3-ADC.patch;
+  }
+  {
+    name = "jg-2adac779ce71-pmic5-gen3-adc-bindings";
+    patch = ./0025-dt-bindings-iio-adc-Add-support-for-QCOM-PMIC5-Gen3-.patch;
+  }
+]
--- packages/x1e80100-linux.nix
+++ packages/x1e80100-linux.nix
@@ -13,5 +13,6 @@ linuxPackagesFor (linuxManualConfig {
     hash = "sha256-WHEmUzzOlOi+Hi2oLUrmYxuFe3r2jNMo3/cwIXCafU8=";
   };
   version = "7.0.12";
+  kernelPatches = import ../kernel-patches/lenovo-t14s-linux-7.0.y/series.nix;
   configfile = ../working_el2.config;
 })
